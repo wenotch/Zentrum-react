95a59d4c5ad39ea75797603a07beadd4ee3cd531
Nwanochie Emmanuel <emmanuel.nwanochie.197850@unn.edu.ng>
2021-10-28 14:40:20 +0100

    updates

---
+++ src/Web.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Web() {
+  return <div></div>;
+}
+
+export default Web;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Web from "./Web";
+import Mobile from "./Mobile";
+import School from "./School";
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="//web-development-job-request" component={Web} />
+      <Route path="/mobile-app-job-request" component={Mobile} />
+      <Route path="/zentrum-school" component={School} />
+    </div>
+  </Router>
 );
-
-
+ReactDOM.render(routing, document.getElementById("root"));
